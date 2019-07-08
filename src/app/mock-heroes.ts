c99b26dba39519f1679ccfa4839b105b398cce06
Margaret <margaret.molefe@symbiotics.co.za>
2019-07-08 07:48:38 +0200

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+import {Hero} from '../app/hero/hero';
+
+export const HEROES: Hero[] = [
+
+{id: 11, name: 'DR Nice'},
+{id: 12, name: 'Narco'},
+{id: 13, name: 'Bombasto'},
+{id: 14, name: 'Celerista'},
+{id: 15, name: 'Magneta'},
+{id: 16, name: 'RubberMan'},
+{id: 17, name: 'Dynama'},
+{id: 18, name: 'DR IQ'},
+{id: 19, name: 'Magma'},
+{id: 20, name: 'Tornado'},
+{id: 21, name: 'Spiderman'},
+{id: 22, name: 'BlackMan'}
+
+];
